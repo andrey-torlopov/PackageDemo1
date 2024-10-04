45453587915cb9efb5bca2eb7049a4ed6e0d3b62
Andrey Torlopov <torlopov.mail@ya.ru>
2024-10-04 12:34:40 +0300

    .

--- Package.swift
+++ Package.swift
@@ -5,17 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "PackageDemo1",
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PackageDemo1",
             targets: ["PackageDemo1"]),
+    ],
+    dependencies: [
+        .package(url: "git@github.com:andrey-torlopov/PackageDemo2.git", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PackageDemo1"),
+            name: "PackageDemo1",
+            dependencies: ["PackageDemo2"]
+        ),
         .testTarget(
             name: "PackageDemo1Tests",
             dependencies: ["PackageDemo1"]),
--- Sources/PackageDemo1/PackageDemo1.swift
+++ Sources/PackageDemo1/PackageDemo1.swift
@@ -1,9 +1,11 @@
+import PackageDemo2
+
 public struct PackageDemo1 {
     public var name: String = "PackageDemo1_2"
     
     public init() { }
     
     public func demo1() -> String {
-        return "\(name)"
+        return "\(name) \(PackageDemo2().demo1())"
     }
 }
