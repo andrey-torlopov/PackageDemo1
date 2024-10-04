52c58dbcbebc1225f15ee6aef3ac14c9f1517193
Andrey Torlopov <torlopov.mail@ya.ru>
2024-10-04 12:17:56 +0300

    .

--- Sources/PackageDemo1/PackageDemo1.swift
+++ Sources/PackageDemo1/PackageDemo1.swift
@@ -1,2 +1,9 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+public struct PackageDemo1 {
+    public var name: String = "PackageDemo1"
+    
+    public init() { }
+    
+    public func demo1() -> String {
+        return "\(name)"
+    }
+}
